4f3c84bc41ef951ab082d8d8eafd8aaf07cd5105
Sara Sailors <sarasailors@gmail.com>
2016-11-30 15:05:43 -0500

    Feat(js, package.json): removing prompt per dylan's request, adding winning combinations as well as a console log to get things started

--- tictactoe.js
+++ tictactoe.js
@@ -6,12 +6,7 @@
 7 | 8 | 9
 */
 
-
-//I am using an npm module prompt to prompt the user for input
-// it will save when you run npm install
-
-var prompt = require('prompt');
-
+//This makes the gameboard with nothing in the squares
 var gameBoard = {
     1: ' ',
     2: ' ',
@@ -23,3 +18,21 @@ var gameBoard = {
     8: ' ',
     9: ' '
 };
+
+///This will end up being the current game board
+function currentGame() {
+    console.log('\n' +
+        ' ' + gameBoard[1] + gameBoard[2] + gameBoard[3] + '\n' +
+        ' ' + gameBoard[4] + gameBoard[5] + gameBoard[6] + '\n' +
+        ' ' + gameBoard[7] + gameBoard[8] + gameBoard[9] + '\n');
+}
+
+//This is a mix of the winning combinations that are possible on the game board
+var winningCombinations = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7], [1, 4, 7],
+    [2, 5, 8], [3, 6, 9]];
+
+//This will let you know that your game is starting!
+console.log('Game is starting YAY!!!!');
+
+yourTurn('X');
+
