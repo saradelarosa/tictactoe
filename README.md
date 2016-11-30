f736e5804da045c7f3ba866fb0fba01f1c744432
Sara Sailors <sarasailors@gmail.com>
2016-11-30 14:46:39 -0500

    Feat(board): adding the game board in js file, readme, npm and gitignore

---
+++ README.md
@@ -0,0 +1,4 @@
+How to run this code from the command line:
+    - clone the repository
+    - npm install
+    -
---
+++ tictactoe.js
@@ -0,0 +1,21 @@
+/* This is a tictactoe game - players 'X' and 'O' should put their number on the command line using a - i
+1 | 2 | 3
+---------
+4 | 5 | 6
+---------
+7 | 8 | 9
+*/
+
+
+
+var gameBoard = {
+    1: ' ',
+    2: ' ',
+    3: ' ',
+    4: ' ',
+    5: ' ',
+    6: ' ',
+    7: ' ',
+    8: ' ',
+    9: ' '
+};
